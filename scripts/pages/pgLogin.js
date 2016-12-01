ac25dc249526124fab9de81a6995ed59177aa5cd
Atakan Eser <atakan.eser@icloud.com>
2016-11-30 23:44:11 -0500

    Removed sample login data

--- scripts/pages/pgLogin.js
+++ scripts/pages/pgLogin.js
@@ -33,7 +33,7 @@
         width: '80%',
         height: '8%',
         placeHolder: 'Username',
-        text: 'test',
+        text: '',
         horizontalGap: 15,
         roundedEdge: 0
     });
@@ -44,7 +44,7 @@
         width: '80%',
         height: '8%',
         placeHolder: 'Password',
-        text: 'Qwe12345',
+        text: '',
         isPassword: true,
         horizontalGap: 15,
         roundedEdge: 0
