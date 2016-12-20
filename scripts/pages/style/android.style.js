f88d6115a8a5dff8dc68d6c89ec67d9de02332b3
Atakan <smartface92@users.smartfacecloud.io>
2016-12-20 03:13:59 +0000

    style files

---
+++ scripts/pages/style/android.style.js
@@ -0,0 +1,28 @@
+module.exports = {
+  ".pgStatus": {
+    ".imgHome": {
+      height: '42.10937%'
+    },
+    ".imgAvatar": {
+      width: '80dp',
+      height: '80dp'
+    },
+    ".lblFullName": {
+      top: '28%',
+      height: '5%'
+    },
+    ".imgDetail": {
+      width: '12dp',
+      height: '20dp'
+    },
+    ".lblNewRequestTextDate": {
+      left: '21%',
+      top: '90%'
+    },
+    ".imgAdd": {
+      left: '78.2777%',
+      width: 61,
+      height: 64
+    }
+  }
+};
---
+++ scripts/pages/style/ios.style.js
@@ -0,0 +1,28 @@
+module.exports = {
+  ".pgStatus": {
+    ".imgHome": {
+      height: '42.05397%'
+    },
+    ".imgAvatar": {
+      width: 80,
+      height: 80
+    },
+    ".lblFullName": {
+      top: '29.4587%',
+      height: '3%'
+    },
+    ".imgDetail": {
+      width: 12,
+      height: 20
+    },
+    ".lblNewRequestTextDate": {
+      left: '29.5%',
+      top: '89.45%'
+    },
+    ".imgAdd": {
+      left: '78.2666%',
+      width: 63,
+      height: 66
+    }
+  }
+};
