bd5e55f54a0aff7cb2099957244bb7c9cce0c6f7
Atakan <smartface92@users.smartfacecloud.io>
2016-12-21 05:15:06 +0000

    styler refactoring

---
+++ scripts/pages/style/pgOutOfOffice.style.js
@@ -0,0 +1,101 @@
+module.exports = {
+    ".textLeft": {
+        textAlignment: SMF.UI.TextAlignment.LEFT,
+        multipleLine: false
+    },
+    ".textCenter": {
+        textAlignment: SMF.UI.TextAlignment.CENTER,
+        multipleLine: false
+    },
+    ".12pt": {
+        font: {
+            size: '7pt',
+            bold: false
+        },
+    },
+    ".7pt": {
+        font: {
+            size: '7pt',
+            bold: false
+        },
+    },
+    ".pgOutOfOffice": {
+        ".horizontalRectangle": {
+            left: 0,
+            width: '100%',
+            height: 1,
+            fillColor: '#e7e7e7',
+            borderWidth: 0,
+            roundedEdge: 0
+        },
+        ".verticalRectangle": {
+            left: '49.90%',
+            width: '100%',
+            height: 1,
+            fillColor: '#e7e7e7',
+            borderWidth: 0,
+            roundedEdge: 0
+        },
+        ".imgAvatar": {
+            image: '',
+            left: '5.3333%',
+            top: 0,
+            width: '14.4%',
+            height: '8.0959%',
+            imageFillType: SMF.UI.ImageFillType.ASPECTFIT
+        },
+        ".lblFullName": {
+            left: '4.5333%',
+            top: 0,
+            width: '53.3333%',
+            height: '4.7376%',
+            fontColor: '#27bc66'
+        },
+        ".lblTeamRole": {
+            left: '4.5333%',
+            top: 0,
+            width: '53.3333%',
+            height: '3.4482%',
+            fontColor: '#444444'
+        },
+        ".swtOutOfOffice": {
+            left: '76.5333%',
+            checked: false,
+            onTintColor: '#248afd',
+            tintColor: '#248afd'
+        },
+        ".lblOOOStatusTitle": {
+            left: '67.2%',
+            top: 0,
+            width: '32.8%',
+            height: '2.9985%',
+            fontColor: '#a0a0a0'
+        },
+        ".lblOOOStatusText": {
+            left: '67.2%',
+            top: 0,
+            width: '32.8%',
+            height: '2.9985%',
+            fontColor: '#37404a'
+        },
+        ".lblStart": {
+            left: '4.5333%',
+            width: '17%',
+            fontColor: '#248afd'
+        },
+        ".lblStartDate": {
+            left: '4.5333%',
+            width: '37.3333%',
+            fontColor: '#4a4a4a'
+        },
+        ".cntSelectStartDate": {
+            left: '4.5333%',
+            width: '37.3333%',
+            height: '6.5967%',
+            backgroundTransparent: true,
+            touchEnabled: true,
+            borderWidth: 0,
+            roundedEdge: 0
+        }
+    }
+};
