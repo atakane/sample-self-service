be0007d24e42fc232c895f837d9b66605f19b949
Atakan <smartface92@users.smartfacecloud.io>
2016-12-21 19:35:08 +0000

    styler implementation

---
+++ scripts/pages/style/colors.js
@@ -0,0 +1,11 @@
+// Color names are based on or inspired by http://www.color-hex.com/
+module.exports = {
+    GrayLighter: '#e7e7e7',
+    GrayLight: '#a0a0a0',
+    Gray29: '#4a4a4a',
+    Green: '#27bc66',
+    GrayDark: '#444444',
+    BlueMedium: '#248afd',
+    BlueDark: '#37404a',
+    White: SMF.UI.Color.WHITE
+}
