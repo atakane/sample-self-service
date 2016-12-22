adaf3ad3ba342b6b34d61133f8529a1083aa7fe8
Atakan <smartface92@users.smartfacecloud.io>
2016-12-22 00:21:23 +0000

    styler implementation for pgOutOfOffice Completed

---
+++ scripts/pages/style/generic.style.js
@@ -0,0 +1,37 @@
+module.exports = {
+    ".textLeft": {
+        textAlignment: SMF.UI.TextAlignment.LEFT,
+        multipleLine: false
+    },
+    ".textRight": {
+        textAlignment: SMF.UI.TextAlignment.RIGHT,
+        multipleLine: false
+    },
+    ".textCenter": {
+        textAlignment: SMF.UI.TextAlignment.CENTER,
+        multipleLine: false
+    },
+    ".16pt": {
+        font: {
+            size: '16pt',
+            bold: false
+        },
+    },
+    ".12pt": {
+        font: {
+            size: '12pt',
+            bold: false
+        },
+    },
+    ".7pt": {
+        font: {
+            size: '7pt',
+            bold: false
+        },
+    },
+    ".bold": {
+        font: {
+            bold: true
+        },
+    }
+}
